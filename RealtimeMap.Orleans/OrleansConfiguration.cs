63725bedc2d5944fb7f86f0739ab61ec375437d9
Ryszard Tarajkowski <ryszard.tarajkowski@skyrise.tech>
2022-02-23 16:23:28 +0100

    Migrating to Orleans 4 alpha - grain activation working, serialization not yet working.

--- RealtimeMap.Orleans/OrleansConfiguration.cs
+++ RealtimeMap.Orleans/OrleansConfiguration.cs
@@ -28,24 +28,6 @@ public static class OrleansConfiguration
             .ConfigureLogging(logging => logging
                 .AddSerilog()
             )
-            .Configure<GrainTypeOptions>(options =>
-            {
-                options.Interfaces.Add(typeof(IGeofenceGrain));
-                options.Classes.Add(typeof(GeofenceGrain));
-                
-                options.Interfaces.Add(typeof(IOrganizationGrain));
-                options.Classes.Add(typeof(OrganizationGrain));
-
-                options.Interfaces.Add(typeof(IUserGrain));
-                options.Classes.Add(typeof(UserGrain));
-
-                options.Interfaces.Add(typeof(IVehicleGrain));
-                options.Classes.Add(typeof(VehicleGrain));
-            })
-            // .ConfigureApplicationParts(parts => parts
-            //     .AddApplicationPart(typeof(VehicleGrain).Assembly)
-            //     .WithReferences()
-            // )
         );
     }
 }
